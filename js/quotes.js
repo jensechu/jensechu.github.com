89526dc45ac841238a669eb181228c27a2ea9728
Jensen <jensechu@gmail.com>
2012-12-15 22:05:24 -0800

    Fixed a missing comma in the JSON

--- js/quotes.js
+++ js/quotes.js
@@ -1,23 +1,24 @@
 function random_quote(){
+    console.log("random quote is running");
+
     $.getJSON("/data/quotes.json", function(data) {
-	console.log("test");
+	console.log("hellooooo the json is hurr");
+
 	var quotes = data.quotes;
 	console.log(quotes);
 
-
 	var random = quotes[Math.floor(Math.random() * quotes.length)];
-
 	console.log(random);
 
 	$(".random-quote").text(random.quote);
 	$(".random-author").text(random.author);
 
-    });
+    })
+   .error(function(j, t, e) { console.log(e);})
     return false;
 }
 
 $(document).ready(function(){
     random_quote();
-
     $("#random-quote").click(random_quote);
 });
